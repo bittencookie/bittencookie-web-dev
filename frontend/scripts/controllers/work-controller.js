1a4d21c32a350f6f9da183e5946d2274229a6d26
Pedro Tavares <p.miguel.tr@gmail.com>
2015-04-07 21:47:37 +0100

    work page improved + works info text

--- frontend/scripts/controllers/work-controller.js
+++ frontend/scripts/controllers/work-controller.js
@@ -1,7 +1,9 @@
 var works = require('../works');
 
-module.exports = function ContactsController($scope) {
+module.exports = function ContactsController($scope, $stateParams, $sce) {
     'use strict';
     $scope.works = works;
+    $scope.work = works[$stateParams.work];
+    $scope.work.info = $sce.trustAsHtml($scope.work.info);
     console.log('work controller');
 };
--- frontend/scripts/works.js
+++ frontend/scripts/works.js
@@ -3,11 +3,71 @@ module.exports = {
     "keepup": {
         "logo": "keepup",
         "background": "background-image:url(\"/img/macbook.jpg\")",
-        "image": "/img/macbook.jpg"
+        "image": "/img/macbook.jpg",
+        "info": "A Diabetes Mellitus é uma doença caraterizada por um aumento anormal da glicose no sangue. Quando não é tratada adequadamente, podem ocorrer complicações perigosas para a saúde e bem estar dos doentes.\n" +
+                "A Diabetes é uma doença bastante comum no mundo abrangendo cerca de 7,6% da população adulta (entre 30 e 69 anos). Embora não haja ainda uma cura definitiva para a diabetes, há vários tratamentos disponíveis que, quando seguidos de forma regular, proporcionam saúde e qualidade de vida para o paciente portador." +
+                "A KeepUp é uma aplicação para sistema operativo Android que procura dar um estilo de vida mais saudável aos pacientes com Diabetes Mellitus."
     },
     "sosshake": {
         "logo": "SOS Shake",
         "background": "background-image:url(\"/img/macbook.jpg\")",
-        "image": "/img/macbook.jpg"
+        "image": "/img/macbook.jpg",
+        "info": "<p>O projeto SOS Shake consiste num sistema de notificação que, através do toque vibratório do smartphone e notificações, avisa os utilizadores de situações de alerta, emergência ou outro tipo de avisos em locais públicos ou privados. </p>" +
+                "<p>Objetivos:</p>" +
+                "<p>- Contribuir para o aumento dos níveis de independência no dia-a-dia de pessoas com deficiência auditiva ou défice elevado de audição pois são o público mais vulnerável em situações onde os avisos sonoros são mais utilizados. </p>" +
+                "<p>- Oferecer ao público-alvo a possibilidade de se deslocar sozinho a locais públicos ou privados e estar numa divisão a trabalhar ou à espera da sua vez sem ter de estar acompanhado por outra pessoa. </p>" +
+                "<p>- Notificar o utilizador quando o smartphone detetar o som de uma sirene de alerta, informando o mesmo para se dirigir à saída de emergência.</p>" +
+                "<p>- Notificar o utilizador quando o smartphone detetar uma mensagem a avisar que chegou a sua vez, por exemplo quando o mesmo se encontra numa sala de espera. .</p>"
+    },
+    "glintt": {
+        "logo": "Glintt",
+        "background": "background-image:url(\"/img/macbook.jpg\")",
+        "image": "/img/macbook.jpg",
+        "info": "<p>Atualmente o apoio ao cliente, na área da saúde, é feito através de call-centers. Esta" +
+                "abordagem provou ser ineficaz, já que apenas uma parte reduzida dos clientes recorre a este" +
+                "serviço, acabando por se dirigir aos balcões sempre que quer comunicar com as instituições.</p>" +
+                "<p>Este tipo de serviços tem ainda outras limitações, como o possível custo da chamada e a" +
+                "ausência de um mecanismo para partilhar documentos. Tudo isto leva a que os clientes evitem" +
+                "o contacto, o que diminui o nível de satisfação dos mesmos. As instituições não recebem" +
+                "feedback sobre os seus serviços (não obtendo informação das redes sociais).</p>" +
+
+                "<p>Objetivos:</p>" +
+                "<p>- Resolver problemas substituindo o telefone pela Internet.</p>" +
+                "<p>- Tornar o processo mais eficaz, mas, sobretudo, mais social.</p>" +
+                "<p>- Associar as redes sociais a este serviço permitindo partilha de ficheiros através de" +
+                "sistemas de armazenamento na cloud, assim permitirá aos clientes contactar com o" +
+                "apoio ao cliente através de ferramentas que já conhecem e se sentem à vontade.</p>" +
+                "<p>- Aumentar o nível de satisfação dos clientes para que recorreram mais frequentemente " +
+                "aos centros de apoio.</p>" +
+                "<p>- Dar às empresas informação em tempo real do que as pessoas dizem sobre elas.</p>"
+    },
+    "escolaglobal": {
+        "logo": "Escola Global",
+        "background": "background-image:url(\"/img/macbook.jpg\")",
+        "image": "/img/macbook.jpg",
+        "info": "<p>A programação estimula o pensamento lógico, a criatividade e a capacidade de resolver" +
+                "problemas, de uma forma lúdica e divertida.</p>" +
+                "<p>Nesta perspetiva, aprender a programar pode configurar uma importante ferramenta " +
+                "interdisciplinar na compreensão e aquisição do conhecimento em disciplinas tão variadas como " +
+                "a Matemática, o Português, a História ou as Ciências Naturais.</p>" +
+                "<p>É precisamente nesse sentido que é necessário, cada vez mais, envolver a tecnologia no " +
+                "processo de aprendizagem dos alunos, como forma de potenciar a sua educação e o seu " +
+                "desenvolvimento.</p>" +
+                "<p>A Bitten Cookie dá um passo pioneiro em Portugal ao introduzir aulas de programação desde a " +
+                "pré-escola até ao 8º ano. Pensados para cada uma das faixas etárias, as aulas contêm " +
+                "conteúdos gráficamente atrativos abrangidos por uma história que dará às crianças e jovens " +
+                "vontade de aprender mais.</p>" +
+                "<p>A escolaglobal®, parceira da Bitten Cookie, grupo formado por duas instituições de ensino " +
+                "particular, introduzirá estes conteúdos em aulas semanais já a partir do próximo ano letivo " +
+                "fazendo uso de uma plataforma de e-learning inovadora.</p>" +
+                "<p>Objetivos:</p>" +
+                "<p>- Introdução ao raciocínio lógico no pré-escolar e 1º ano com pequenos robôs " +
+                "programáveis.</p>" +
+                "<p>- Exploração de conceitos de programação por meio visual com utilização de Scratch no " +
+                "2º ano.</p>" +
+                "<p>- Aprendizagem de programação em Python em relação estreita com conteúdos de " +
+                "outras disciplinas.</p>" +
+                "<p>- Desenvolvimento de plataforma de e-learning adaptada a crianças e jovens da " +
+                "escolaglobal®.</p>"
     }
 };
