99fbbcdccdd4901fdcd317ea22742d813ef0ebf5
Gabriel Candal <gabrielcandal@gmail.com>
2015-05-26 12:24:35 +0100

    Minor text changes. Mailto.

--- frontend/scripts/controllers/contacts-controller.js
+++ frontend/scripts/controllers/contacts-controller.js
@@ -5,9 +5,24 @@
 
 module.exports = function ContactsController($scope, $state) {
     'use strict';
+    $scope.destiny = {
+        email: "contact@bittencookie.pt",
+        subject: "Site contact form"
+    };
+
     $scope.data = {
         name: '',
         address: '',
         message: ''
+    };
+
+    var concatBodyAddress = function () {
+        return "From: " + $scope.data.name + ", " + $scope.data.address
+            + "\n" + $scope.data.message;
+    };
+
+    $scope.mailtoHref = function () {
+        return encodeURI('mailto:' + $scope.destiny.email + '?subject='
+            + $scope.destiny.subject + '&body=' + concatBodyAddress());
     };
 };
